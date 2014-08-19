06091590d90934b748a5a03a22dbf6a2176a4efc
Nicholas <mario.wardhana@gmail.com>
2014-08-19 21:51:07 +0800

    Variables for enemies

--- Assets/Game.js
+++ Assets/Game.js
@@ -1,9 +1,13 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
-
-function Update () {
-
-}
+private var remainingEnemy: int;
+private var currentEnemy: int;
+public var increaseRate: int = 1;
+public var moveSpeedIncRate: float = 0.5f;
+public var shootRateIncRate: float = 0.2f;
+public var spawnRateIncRate: float = 0.5f;
+public var initShootRate: float = 3.0f;
+public var initMoveSpeed: float = 0.5f;
+public var initSpawnRate: float = 3.0f;
+private var score: int = 0;
+private var level: int = 0;
